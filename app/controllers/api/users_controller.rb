3da11508b49b216dd67cacb864b7bc80cfb34712
ChrisG <christophergarthright@gmail.com>
2020-02-11 18:04:02 -0500

    anotherone

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,10 +1,12 @@
 class Api::UsersController < ApplicationController 
     def create 
         @user = User.new(user_params)
-        login!(@user)
-        render :show
-    else
-        render json: @user.errors.full_messages, status: 422
+        if @user.save 
+            login!(@user)
+            render :show
+        else
+            render json: @user.errors.full_messages, status: 422
+        end
     end
 
     def update 
