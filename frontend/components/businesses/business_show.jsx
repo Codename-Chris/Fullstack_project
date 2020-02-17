66c13f5a44bde819fcec4ed037354da652facd55
ChrisG <christophergarthright@gmail.com>
2020-02-17 17:55:51 -0500

    anotherone

--- frontend/components/businesses/business_show.jsx
+++ frontend/components/businesses/business_show.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class BusinessShow extends React.Component {
     constructor(props) {
@@ -8,6 +9,9 @@ class BusinessShow extends React.Component {
     render() {
         return (
             <div>
+                <header>
+                    <Link to="/" className="header-link"><h1>Celp <img src="https://s3-media0.fl.yelpcdn.com/assets/srv0/yelp_styleguide/dcb526e86d86/assets/img/logos/burst_desktop_xsmall_outline@2x.png" className="header-logo" /></h1></Link>
+                </header>
                 <h3>This will be the Business Show Page!</h3>
             </div>
         )
