eb0670fc78a565b48e6b0996dab5c86fd433b02e
ChrisG <christophergarthright@gmail.com>
2020-05-28 23:44:44 -0400

    fixing some styling

--- frontend/components/businesses/businesses_index_item.jsx
+++ frontend/components/businesses/businesses_index_item.jsx
@@ -97,33 +97,35 @@ const BusinessIndexItem = (props) => {
     }
     
     return (
-        <div className="biz-index-item">
-            <Link className="biz-pic" to={`/businesses/${props.business.id}`}><img className="biz-img-pic" src={props.business.photoUrls[0]}/></Link>
-            <div className="biz-index-item-inner">
-                
-                <Link className="biz-name" to={`/businesses/${props.business.id}`}> {props.business.name}</Link>
-                {getStars()}
-                {/* <p className="biz-rating">
-                    <i className="fas fa-star" id="biz-stars" />
-                    <i className="fas fa-star" id="biz-stars"/>
-                    <i className="fas fa-star" id="biz-stars"/>
-                    <i className="fas fa-star" id="biz-stars"/>
-                    <i className="fas fa-star" id="biz-stars"/>
-                     {props.reviews.filter(review => {
-                         return ( review.business_id === props.business.id)
-                     }).length} Reviews 
-                </p> */}
-                
-                <p className="biz-category">{props.business.price} • {props.business.category}</p>
-                {/* <p> more info </p> */}
-            <p className="biz-description"> {props.business.description} </p>
+        <Link  className="biz-index-item-link" to={`/businesses/${props.business.id}`}>
+            <div className="biz-index-item">
+                <Link className="biz-pic" to={`/businesses/${props.business.id}`}><img className="biz-img-pic" src={props.business.photoUrls[0]}/></Link>
+                <div className="biz-index-item-inner">
+                    
+                    <Link className="biz-name" to={`/businesses/${props.business.id}`}> {props.business.name}</Link>
+                    {getStars()}
+                    {/* <p className="biz-rating">
+                        <i className="fas fa-star" id="biz-stars" />
+                        <i className="fas fa-star" id="biz-stars"/>
+                        <i className="fas fa-star" id="biz-stars"/>
+                        <i className="fas fa-star" id="biz-stars"/>
+                        <i className="fas fa-star" id="biz-stars"/>
+                        {props.reviews.filter(review => {
+                            return ( review.business_id === props.business.id)
+                        }).length} Reviews 
+                    </p> */}
+                    
+                    <p className="biz-category">{props.business.price} • {props.business.category}</p>
+                    {/* <p> more info </p> */}
+                <p className="biz-description"> {props.business.description} </p>
+                </div>
+                <div className="biz-add-div">
+                    <p>{props.business.phone}</p>
+                    <p>{props.business.address}</p>
+                    <p className="biz-address">New York, NY</p>
+                </div>
             </div>
-            <div className="biz-add-div">
-                <p>{props.business.phone}</p>
-                <p>{props.business.address}</p>
-                <p className="biz-address">New York, NY</p>
-            </div>
-        </div>
+        </Link>
     )
 };
 
